77843c5c58b570ebc916afd7e84723c23024920b
Metarock <esang037@gmail.com>
2021-05-24 22:11:40 +1200

    validate if user exists

--- client/src/utils/refreshToken.js
+++ client/src/utils/refreshToken.js
@@ -3,20 +3,22 @@
 /* eslint-disable import/prefer-default-export */
 /* eslint-disable no-unused-vars */
 /* eslint-disable import/no-extraneous-dependencies */
-export const refreshTokenSetup = res => {
+
+export const refreshTokenSetup = (res) => {
   // Timing to renew access token
-  let refreshTiming = (res.tokenObj.expires_in || 3600 - 5 * 60) * 1000
+  let refreshTiming = (res.tokenObj.expires_in || 3600 - 5 * 60) * 1000;
 
   const refreshToken = async () => {
-    const newAuthRes = await res.reloadAuthResponse()
-    refreshTiming = (newAuthRes.expires_in || 3600 - 5 * 60) * 1000
-    console.log("newAuthRes:", newAuthRes)
+    const newAuthRes = await res.reloadAuthResponse();
+    refreshTiming = (newAuthRes.expires_in || 3600 - 5 * 60) * 1000;
+    console.log('newAuthRes:', newAuthRes);
+    // saveUserToken(newAuthRes.access_token);  <-- save new token
+    localStorage.setItem('authToken', newAuthRes.id_token);
 
-    console.log("new auth Token", newAuthRes.id_token)
+    // Setup the other timer after the first one
+    setTimeout(refreshToken, refreshTiming);
+  };
 
-    // setup the other timer after the first one
-    setTimeout(refreshToken, refreshTiming)
-  }
-  // setup the first refresh timer
-  setTimeout(refreshToken, refreshTiming)
-}
+  // Setup first refresh timer
+  setTimeout(refreshToken, refreshTiming);
+};
