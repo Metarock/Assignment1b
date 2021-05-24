e06aa049bb8e143c56bd0ef4e2596dc1c2251562
Metarock <esang037@gmail.com>
2021-05-24 21:03:58 +1200

    registration google completed

--- client/src/components/Logout.js
+++ client/src/components/Logout.js
@@ -3,32 +3,32 @@
 /* eslint-disable import/prefer-default-export */
 /* eslint-disable no-unused-vars */
 /* eslint-disable import/no-extraneous-dependencies */
-import React from 'react'
-import { useGoogleLogout } from 'react-google-login'
+import React from "react"
+import { useGoogleLogout } from "react-google-login"
 
-const clientId = '920604998733-jlo1s9knrt4kpaet080pltjt8orengkk.apps.googleusercontent.com'
-
+const clientId =
+  "920604998733-jlo1s9knrt4kpaet080pltjt8orengkk.apps.googleusercontent.com"
 
 export const Logout = () => {
-    const onLogoutSuccess = () => {
-        alert('Logged out successfully')
-    }
+  const onLogoutSuccess = () => {
+    alert("Logged out successfully")
+  }
 
-    const onFailure = () => {
-        console.log('Handle Failure cases')
-    }
+  const onFailure = () => {
+    console.log("Handle Failure cases")
+  }
 
-    const { signOut } = useGoogleLogout({
-        clientId,
-        onLogoutSuccess,
-        onFailure
-    })
+  const { signOut } = useGoogleLogout({
+    clientId,
+    onLogoutSuccess,
+    onFailure
+  })
 
-    return (
-        <button type='button' onClick={signOut} className='button'>
-            <img src='icons/google.svg' alt='google login' className='icon'></img>
+  return (
+    <button type="button" onClick={signOut} className="button">
+      <img src="icons/google.svg" alt="google login" className="icon"></img>
 
-            <span className='buttonText'>Sign Out</span>
-        </button>
-    )
+      <span className="buttonText">Sign Out</span>
+    </button>
+  )
 }
--- client/src/utils/refreshToken.js
+++ client/src/utils/refreshToken.js
@@ -3,22 +3,20 @@
 /* eslint-disable import/prefer-default-export */
 /* eslint-disable no-unused-vars */
 /* eslint-disable import/no-extraneous-dependencies */
-export const refreshTokenSetup = (res) => {
-    // Timing to renew access token 
-    let refreshTiming = (res.tokenObj.expires_in || 3600 - 5 * 60) * 1000
+export const refreshTokenSetup = res => {
+  // Timing to renew access token
+  let refreshTiming = (res.tokenObj.expires_in || 3600 - 5 * 60) * 1000
 
-    const refreshToken = async () => {
-        const newAuthRes = await res.reloadAuthResponse()
-        refreshTiming = (newAuthRes.expires_in || 3600 - 5 * 60) * 1000
-        console.log('newAuthRes:', newAuthRes)
+  const refreshToken = async () => {
+    const newAuthRes = await res.reloadAuthResponse()
+    refreshTiming = (newAuthRes.expires_in || 3600 - 5 * 60) * 1000
+    console.log("newAuthRes:", newAuthRes)
 
-        console.log('new auth Token', newAuthRes.id_token)
+    console.log("new auth Token", newAuthRes.id_token)
 
-        // setup the other timer after the first one
-        setTimeout(refreshToken, refreshTiming)
-
-    }
-    // setup the first refresh timer
+    // setup the other timer after the first one
     setTimeout(refreshToken, refreshTiming)
-
+  }
+  // setup the first refresh timer
+  setTimeout(refreshToken, refreshTiming)
 }
