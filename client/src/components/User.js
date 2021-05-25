103b52071a1ddbb71e173ae4e02ee71ba70d58f5
Metarock <esang037@gmail.com>
2021-05-25 14:03:48 +1200

    view users

---
+++ client/src/components/User.js
@@ -0,0 +1,20 @@
+/* eslint-disable no-console */
+/* eslint-disable no-restricted-exports */
+/* eslint-disable import/prefer-default-export */
+/* eslint-disable react/prop-types */
+import React from "react"
+
+export const User = ({ userData }) => {
+  const user = userData
+  console.log(user.googleId)
+
+  return (
+    <tr>
+      <td>{user.googleId}</td>
+      <td>{user.name}</td>
+      <td>{user.email}</td>
+      <td>{user.imageUrl}</td>
+      <td>{user.role}</td>
+    </tr>
+  )
+}
